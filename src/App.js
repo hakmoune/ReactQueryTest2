9913e6e9e90105cfed76c9f6f3ae5b46ae37dc6b
mehdi <mehdi.hakmoune@gmail.com>
2024-03-16 01:08:09 +0000

    Simple Test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { getProducts, deleteProduct } from "./api/api";
+import { useQuery, useQueryClient, useMutation } from "react-query";
 
 function App() {
+  const queryClient = useQueryClient();
+
+  // const { data, isLoading } = useQuery(["Products"], getProducts, {
+  //   refetchOnWindowFocus: false
+  // });
+
+  const { data, isLoading } = useQuery({
+    queryFn: getProducts,
+    queryKey: "Products",
+    refetchOnWindowFocus: false
+  });
+
+  // if (isLoading) return <p>Loading...</p>;
+
+  const { products } = data || [];
+  console.log("data", products);
+
+  // Delete Product
+
+  // const deleteMutation = useMutation(deleteProduct, {
+  //   onSuccess: () => {
+  //     queryClient.invalidateQueries("Products");
+  //   }
+  // });
+
+  const deleteMutation = useMutation({
+    mutationFn: deleteProduct,
+    onSuccess: () => {
+      queryClient.invalidateQueries("Products");
+    }
+  });
+
+  console.log("mutation", deleteMutation.data);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Product List</h1>
+      {isLoading ? (
+        <p>Loading...</p>
+      ) : (
+        products.map(product => (
+          <Product
+            key={product.id}
+            product={product}
+            deleteMutation={deleteMutation}
+          />
+        ))
+      )}
     </div>
   );
 }
+
+const Product = ({ product, deleteMutation }) => {
+  return (
+    <div>
+      <p>{product.title}</p>
+      <button onClick={() => deleteMutation.mutate(product)}>Delete !</button>
+    </div>
+  );
+};
 
 export default App;
